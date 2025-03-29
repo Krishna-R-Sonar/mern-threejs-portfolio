594f35c43e4a260e4a0189519eb2e90166c607aa
Krishna-R-Sonar <144574990+Krishna-R-Sonar@users.noreply.github.com>
2025-03-29 13:28:23 +0530

    Create LazyLoad.jsx

---
+++ client/src/components/LazyLoad.jsx
@@ -0,0 +1,32 @@
+import { useRef, useEffect, useState } from "react";
+
+const LazyLoad = ({ children }) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+          observer.unobserve(ref.current);
+        }
+      },
+      { threshold: 0.1 }
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, []);
+
+  return <div ref={ref}>{isVisible ? children : null}</div>;
+};
+
+export default LazyLoad;
